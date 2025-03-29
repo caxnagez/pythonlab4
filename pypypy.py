d0f9182f06ab5a28f5ca3f82edf137ea373e4cf0
Ilya Malashkevich <overkillzet@gmail.com>
2025-03-29 20:00:10 +0700

    fix

--- pypypy.py
+++ pypypy.py
@@ -53,11 +53,26 @@ def print_operation_table(operation, num_rows=9, num_columns=9):
             print()
 print_operation_table(lambda x, y: x*y, 5)
 #Задание 9
-
-# я хз че сделать как потом сделаю
-
+def ask_password(login: str, password: str, success: callable, failure: callable):
+    count_v, count_c, pos_c = 0, 0, 0
+    for pos, symbol in enumerate(password.lower()):
+        if symbol in "aeiouy":
+            count_v += 1
+        elif symbol in login.lower():
+            count_c, pos_c = 0 if pos_c > pos else count_c + 1, pos
+    if count_v < 3 and count_c < 3:
+        failure(login, "Everything is wrong")
+    elif count_v < 3:
+        failure(login, "Wrong number of vowels")
+    elif count_c < 3:
+        failure(login, "Wrong consonants")
+    else:
+        success(login)
+ask_password("login", "1",
+    lambda login: print(f"SUCCES: #{login}"),
+    lambda login, message: print(f"FAILURE: #{login} | info: {message}"))
 #Задание 10 standard features
-
+print(*sorted(input().split(), key=str.lower))
 #Задание 11
 list2 = [3, 6, -8, 2, -78, 1, 23, -45, 9]
 list2.sort()
@@ -68,6 +83,17 @@ print(list2)
 #Задание 15
 #Задание 16
 #Задание 17
+def check_password(foo):
+    def check():
+        password = input()
+        if password != '123':
+            print('В доступе отказано')
+            return None
+        foo()
+    return check()
+@check_password
+def hi():
+    print('Hi')
 #Задание 18
 #Задание 19
 
